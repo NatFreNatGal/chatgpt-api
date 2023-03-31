66703ee0648063c2fd7d05fe9cabc9c86bfca811
freistli <freistli@microsoft.com>
2023-03-31 17:48:01 +0800

    update az functon for openai, use release packages

--- demos/demo-azurefunction/openai/index.ts
+++ demos/demo-azurefunction/openai/index.ts
@@ -26,40 +26,13 @@ class MyOpenAI {
   }
 
   public async initOpenAI() {
-    const clientOptions = {
-      // (Optional) Parameters as described in https://platform.openai.com/docs/api-reference/completions
-      modelOptions: {
-        // The model is set to text-chat-davinci-002-20230126 by default, but you can override
-        // it and any other parameters here
-        model: 'text-davinci-002-render'
-      },
-      // (Optional) Set a custom prompt prefix. As per my testing it should work with two newlines
-      // promptPrefix: 'You are not ChatGPT...\n\n',
-      // (Optional) Set a custom name for the user
-      // userLabel: 'User',
-      // (Optional) Set a custom name for ChatGPT
-      // chatGptLabel: 'ChatGPT',
-      // (Optional) Set to true to enable `console.debug()` logging
-      debug: false
-    }
-    const cacheOptions = {
-      // Options for the Keyv cache, see https://www.npmjs.com/package/keyv
-      // This is used for storing conversations, and supports additional drivers (conversations are stored in memory by default)
-      // For example, to use a JSON file (`npm i keyv-file`) as a database:
-      // store: new KeyvFile({ filename: 'cache.json' }),
-    }
     this.api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
   }
 
-  public async callOpenAI(
-    prompt: string,
-    messageId: string,
-    conversationId: string
-  ): Promise<any> {
+  public async callOpenAI(prompt: string, messageId: string): Promise<any> {
     console.log('mid:' + messageId)
-    console.log('cid:' + conversationId)
 
-    if (messageId == '' || conversationId == '') {
+    if (messageId == '') {
       const res = await oraPromise(this.api.sendMessage(prompt), {
         text: prompt
       })
@@ -67,7 +40,6 @@ class MyOpenAI {
     } else {
       const res = await oraPromise(
         this.api.sendMessage(prompt, {
-          conversationId: conversationId,
           parentMessageId: messageId
         }),
         {
@@ -90,8 +62,7 @@ const httpTrigger: AzureFunction = async function (
     console.log(req.body.prompt)
     const result = await MyOpenAI.Instance()?.callOpenAI(
       req.body.prompt,
-      req.body.messageId,
-      req.body.conversationId
+      req.body.messageId
     )
     context.res = {
       // status: 200, /* Defaults to 200 */
